091301387bb0a09d1643f6f503867def455f1556
Shaun Hurley <25695211+shaunthurley@users.noreply.github.com>
2019-04-03 15:19:26 -0400

    ttt update

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,31 +1,131 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Lesson 6</title>
-        <link href="css/style.css" type="text/css" rel="stylesheet">
-    </head>
-    <body>
-        <h1>Tic Tac Toe</h1>
-          <div class="row top">
-            <div id="tl" class="square left"></div>
-            <div id="tc" class="square center"></div>
-            <div id="tr" class="square right"></div>
-        </div>
-        <div class="row middle">
-            <div id="ml" class="square left"></div>
-            <div id="mc" class="square center"></div>
-            <div id="mr" class="square right"></div>
-        </div>
-        <div class="row bottom">
-            <div id="bl"  class="square left"></div>
-            <div id="bc" class="square center"></div>
-            <div id="br" class="square right"></div>
-        </div>
+var topLeft = document.getElementById("tl");
+var topCenter = document.getElementById("tc");
+var topRight = document.getElementById("tr");
+var middleLeft = document.getElementById("ml");
+var middleCenter = document.getElementById("mc");
+var middleRight = document.getElementById("mr");
+var bottomLeft = document.getElementById("bl");
+var bottomCenter = document.getElementById("bc");
+var bottomRight = document.getElementById("br");
+
+var allElements = [topLeft, topCenter, topRight, middleLeft, middleCenter, middleRight, bottomLeft, bottomCenter, bottomRight];
+
+var remainingLocations = ["tl", "tc", "tr", "ml", "mc", "mr", "bl", "bc", "br"];
+var playerLocations = [];
+var computerLocations = []
+
+for(var i=0; i<allElements.length; i++) {
+    allElements[i].addEventListener("click", makeX);
+    allElements[i].addEventListener("mouseover", makeBlack);
+}
+
+function makeX() {
+    var thisElementID = this.attributes["id"].value;
+    var thisElement = document.getElementById(thisElementID);
+    thisElement.textContent = "X";
+
+    var indexOfLocation = remainingLocations.indexOf(thisElementID);
+    if(indexOfLocation != -1) {
+       remainingLocations.splice(indexOfLocation,1);
+        playerLocations[playerLocations.length] = thisElementID;
+//        alert(playerLocations);
+        makeO();
+        checkForWinner();
+       }
+}
+
+function makeBlack() {
+    var thisElementID = this.attributes["id"].value;
+    var thisElement = document.getElementById(thisElementID);
+    thisElement.className += " black"
+}
+
+function makeO() {
+    var randomIndex = Math.floor(Math.random() * remainingLocations.length);
+    var computerElementID = remainingLocations[randomIndex];
+    var computerElement = document.getElementById(computerElementID);
+    computerElement.textContent = "O";
+
+    remainingLocations.splice(randomIndex,1);
+    computerLocations[computerLocations.length] = computerElementID;
+    checkForWinner();
+}
+
+function checkForWinner() {
+    if()
+}
+
+function checkForThree(locations) {
+    var l = 0;
+    var c = 0;
+    var r = 0;
+    var t = 0;
+    var m = 0;
+    var b = 0;
+    for(var i=0; i<locations; i++) {
+
+    }
+}
 
 
 
-        <script src="js/tictactoe.js"></script>
 
-    </body>
-</html>
+
+
+
+
+//var allElements = [topLeft, topCenter, topRight, middleLeft, middleCenter, middleRight, bottomLeft, bottomCenter, bottomRight];
+//// New
+//var remainingLocations = ["tl", "tc", "tr", "ml", "mc", "mr", "bl", "bc", "br"];
+//var playerLocations = [];
+//var computerLocations = [];
+////
+//for(var i=0; i<allElements.length; i++) {
+//    allElements[i].addEventListener("click", makeX);
+//    allElements[i].addEventListener("mouseover", makeBlack);
+//}
+//
+//function makeX() {
+//    var thisElementID = this.attributes["id"].value;
+//    var thisElement = document.getElementById(thisElementID);
+//    thisElement.textContent = "X";
+//    var indexOfElement = remainingLocations.indexOf(thisElementID);
+//    if(indexOfElement != -1) {
+//        remainingLocations.splice(indexOfElement,1);
+//        playerLocations[playerLocations.length] = thisElementID;
+////        alert(playerLocations.includes("tr"));
+//        makeO();
+//       }
+//
+//}
+//
+//function makeBlack() {
+//    var thisElementID = this.attributes["id"].value;
+//    var thisElement = document.getElementById(thisElementID);
+//    thisElement.className += " black"
+//}
+//// New
+//function makeO() {
+//    var randomIndex = Math.floor(Math.random()*remainingLocations.length);
+//    var computerElementID = remainingLocations[randomIndex]
+//    var computerElement = document.getElementById(computerElementID);
+//    computerElement.textContent = "O";
+//    remainingLocations.splice(randomIndex,1);
+//    computerLocations[computerLocations.length] = computerElementID;
+//    alert(computerLocations);
+//}
+////
+//function checkForWinner() {
+//    if(remainingLocations.length == 0) {
+//       return "Draw"
+//       }
+//    else if() {
+//
+//            }
+//}
+//
+//function checkForThree(locations) {
+//    var rCount = 0;
+//    var lCount = 0;
+//    var mCount = 0
+//}
